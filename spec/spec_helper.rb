45e0411ebc8fb1f81cf872a06d68bde908752d50
Scott Taylor <scott@railsnewbie.com>
2014-07-10 15:06:49 -0700

    enable code coverage with COVERAGE=1 rspec ...

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,7 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'rubygems'
 require 'rspec'
 require 'simplecov'
-# SimpleCov.start
+SimpleCov.start if ENV['COVERAGE'] == '1'
 require 'mm-sluggable'
 
 MongoMapper.database = 'mm-sluggable-spec'
